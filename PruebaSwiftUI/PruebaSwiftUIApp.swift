a3356eeedea9f6d93e50094516344110fa6bf4f3
Richard Narvaez <richard.bvn997@gmail.com>
2021-03-03 14:24:39 -0500

    Initial Commit

---
+++ PruebaSwiftUI/PruebaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaSwiftUIApp.swift
+//  PruebaSwiftUI
+//
+//  Created by Richard Narvaez on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PruebaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
